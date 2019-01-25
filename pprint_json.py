0f7361d2c5c896971a18a73d46c66a7dd32ddd85
Info Supsys <info@supsys.cc>
2019-01-25 12:39:52 +0300

    fix errors

--- pprint_json.py
+++ pprint_json.py
@@ -1,18 +1,25 @@
-import json, sys
+import json, sys, os
 
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding='UTF-8') as f:
+    with open(filepath, 'r', encoding='utf8') as f:
         file_content = f.read()
-    return json.loads(file_content)
+    try:
+        return json.loads(file_content)
+    except:
+        return 'Invalid Json'
 
 
-def pretty_print_json(python_dict):
-    return json.dumps(python_dict, indent=4)
+def pretty_print_json(data_to_json):
+    return json.dumps(data_to_json, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    file_path = sys.argv[1]
+    if sys.argv[1] and os.path.isfile(sys.argv[1]):
+        file_path = sys.argv[1]
+    else:
+        exit('File path not specified or file not exist. Look at README.md for more details')
+
     pretty = pretty_print_json(
          load_data(file_path)
     )
